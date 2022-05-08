7c04df5e20b70df8575321ef56ca0268e9c585ef
Marien Mupenda <marien.mupenda@gmail.com>
2022-05-08 22:39:55 +0200

    restructure the project

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="mt-6">
+        <ul class="list-disc">
+            <li>
+                <Link class="text-blue-500 hover:underline" href="/">Home</Link>
+            </li>
+            <li>
+                <Link class="text-blue-500 hover:underline" href="/about">About</Link>
+            </li>
+            <li>
+                <Link class="text-blue-500 hover:underline" href="/contact">Contact</Link>
+            </li>
+            <li>
+                <Link :data="{foo:'Bar'}" as="button" class="text-blue-500 hover:underline" href="/logout"
+                      method="post">Log out
+                </Link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1 class="text-3xl font-bold">Home</h1>
+    <Nav/>
+</template>
+
+<script>
+
+import Nav from "../components/Navbar";
+
+export default {
+    name: "Welcome",
+    components: {Nav},
+    props: {
+        name: {
+            type: String,
+            default: "Marien"
+        },
+        frameworks: {
+            type: Array,
+            default: null
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
